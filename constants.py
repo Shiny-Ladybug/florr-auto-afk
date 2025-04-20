824f2c4ca174d8a9986aeb2bb931067dc8f8afd0
Shiny-Ladybug <lemondog101@outlook.com>
2025-04-20 15:13:52 +0800

    v1.1.1 pre Update

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ ASSET_REPO = "Shiny-Ladybug/assets"
 PROJECT_REPO = "Shiny-Ladybug/florr-auto-afk"
 VERSION_INFO = "1.1.1"
 VERSION_TYPE = "Release"
-RELEASE_DATE = "2025-04-20 14:54:56"
+RELEASE_DATE = "2025-04-20 15:13:12"
 CHANGELOG = {
     "1.1.1": [
         "fix GUI conflict",
